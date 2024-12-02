79e21e0bf284655507e53fa62953615ddf11a0a4
zackaids <84846343+zackaids@users.noreply.github.com>
2024-12-02 00:01:18 -0500

    Almost done solver

--- solver.py
+++ solver.py
@@ -11,4 +11,61 @@ examplegrid = [
 ]
 
 def solveboard(grid):
+    n = 9
+
+    def isvalid(row, col, num):
+        # Check row and column
+        for i in range(n):
+            if grid[row][i] == num or grid[i][col] == num:
+                return False
+        # Check subgrid
+        subgrid_row, subgrid_col = 3 * (row // 3), 3 * (col // 3)
+        for i in range(subgrid_row, subgrid_row + 3):
+            for j in range(subgrid_col, subgrid_col + 3):
+                if grid[i][j] == num:
+                    return False
+        return True
+
+    def solve(row, col):
+        if row == n:
+            return True
+        if col == n:
+            return solve(row + 1, 0)
+        if grid[row][col] == 0:
+            for i in range(1, 10):
+                if isvalid(row, col, i):
+                    grid[row][col] = i
+                    if solve(row, col + 1):
+                        return True
+                    grid[row][col] = 0
+            return False
+        return solve(row, col + 1)
+
+    return solve(0, 0)
+
+if solveboard(examplegrid):
+    for row in examplegrid:
+        print(row)
+else:
+    print("No Solution")
+   
+print(examplegrid)
+print("helele ")
+
+
+
+    # def backtrack(grid):
+    #     for row in range(9):
+    #         for col in range(9):
+    #             if grid[row][col] == "0":
+    #                 for num in "123456789":
+    #                     if isvalid(grid, row, col, num):
+    #                         grid[row][col] = num
+
+    #                         if backtrack(grid):
+    #                             return True
+    #                         grid[row][col] = "0"
+    #                 return False
+    #     return True
     
+    # return backtrack(grid)
