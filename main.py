0290d1f36b083af02f7e8bd34e2c513aefde9f84
zackaids <zalkar.ai77@gmail.com>
2024-12-03 13:32:03 -0500

    Began connecting main to solver

--- main.py
+++ main.py
@@ -1,49 +1,24 @@
-examplegrid = [
-    [5, 3, 0, 0, 7, 0, 0, 0, 0],
-    [6, 0, 0, 1, 9, 5, 0, 0, 0],
-    [0, 9, 8, 0, 0, 0, 0, 6, 0],
-    [8, 0, 0, 0, 6, 0, 0, 0, 3],
-    [4, 0, 0, 8, 0, 3, 0, 0, 1],
-    [7, 0, 0, 0, 2, 0, 0, 0, 6],
-    [0, 6, 0, 0, 0, 0, 2, 8, 0],
-    [0, 0, 0, 4, 1, 9, 0, 0, 5],
-    [0, 0, 0, 0, 8, 0, 0, 7, 9]
-]
+from solver import getgrids
 
-examplegrid1 = [
-    [5, 3, 4, 6, 7, 8, 9, 1, 2],
-    [6, 7, 2, 1, 9, 5, 3, 4, 8],
-    [1, 9, 8, 3, 4, 2, 5, 6, 7],
-    [8, 5, 9, 7, 6, 1, 4, 2, 3],
-    [4, 2, 6, 8, 5, 3, 7, 9, 1],
-    [7, 1, 3, 9, 2, 4, 8, 5, 6],
-    [9, 6, 1, 5, 3, 7, 2, 8, 0],
-    [2, 8, 7, 4, 1, 9, 6, 3, 5],
-    [3, 4, 5, 2, 8, 6, 1, 7, 9]
-]
+filepath = "sudoku_boards_unsolved.txt"
+unsolvedgrid, solvedgrid = getgrids(filepath)
+print(unsolvedgrid)
+print(solvedgrid)
 
-won_grid = [
-    [5, 3, 4, 6, 7, 8, 9, 1, 2],
-    [6, 7, 2, 1, 9, 5, 3, 4, 8],
-    [1, 9, 8, 3, 4, 2, 5, 6, 7],
-    [8, 5, 9, 7, 6, 1, 4, 2, 3],
-    [4, 2, 6, 8, 5, 3, 7, 9, 1],
-    [7, 1, 3, 9, 2, 4, 8, 5, 6],
-    [9, 6, 1, 5, 3, 7, 2, 8, 4],
-    [2, 8, 7, 4, 1, 9, 6, 3, 5],
-    [3, 4, 5, 2, 8, 6, 1, 7, 9]
-]
 
-def main(grid):
-    state = checkgame(grid)
+def main(grid, solvedgrid):
+    state = checkgame(grid, solvedgrid)
     original_set = checkgrid(grid)
     while state == False:
         changecell(grid, original_set)
-        state = checkgame(grid)
+        state = checkgame(grid, solvedgrid)
     print("Congratulations, you have won!")
 
 # function for win con
-def checkgame(grid):
+def checkgame(grid, solvedgrid):
+    if grid == solvedgrid:
+        return True
+
     def isvalid(group):
         return sorted(group) == list(range(1, 10))
 
@@ -123,4 +98,4 @@ def changecell(grid, myset):
 
     return grid
 
-main(examplegrid1)
+main(unsolvedgrid, solvedgrid)
--- solver.py
+++ solver.py
@@ -1,26 +1,6 @@
-examplegrid = [
-    [5, 3, 0, 0, 7, 0, 0, 0, 0],
-    [6, 0, 0, 1, 9, 5, 0, 0, 0],
-    [0, 9, 8, 0, 0, 0, 0, 6, 0],
-    [8, 0, 0, 0, 6, 0, 0, 0, 3],
-    [4, 0, 0, 8, 0, 3, 0, 0, 1],
-    [7, 0, 0, 0, 2, 0, 0, 0, 6],
-    [0, 6, 0, 0, 0, 0, 2, 8, 0],
-    [0, 0, 0, 4, 1, 9, 0, 0, 5],
-    [0, 0, 0, 0, 8, 0, 0, 7, 9]
-]
-won_grid = [
-    [5, 3, 4, 6, 7, 8, 9, 1, 2],
-    [6, 7, 2, 1, 9, 5, 3, 4, 8],
-    [1, 9, 8, 3, 4, 2, 5, 6, 7],
-    [8, 5, 9, 7, 6, 1, 4, 2, 3],
-    [4, 2, 6, 8, 5, 3, 7, 9, 1],
-    [7, 1, 3, 9, 2, 4, 8, 5, 6],
-    [9, 6, 1, 5, 3, 7, 2, 8, 4],
-    [2, 8, 7, 4, 1, 9, 6, 3, 5],
-    [3, 4, 5, 2, 8, 6, 1, 7, 9]
-]
+import random
 
+# solver
 def solveboard(grid):
     n = 9
 
@@ -57,12 +37,33 @@ def solveboard(grid):
     else:
         return "No Solution"
 
+def readrandomdata(filepath):
+    with open(filepath, "r") as file:
+        lines = file.readlines()
 
-solution = solveboard(examplegrid)
-if solution != "No Solution":
-    for row in solution:
-        print(row)
-else:
-    print(solution)
+    data = random.choice(lines).strip()
 
-print(solution)
+    grid = [[int(data[i * 9 + j]) for j in range(9)] for i in range(9)]
+    return grid
+
+def getgrids(filepath):
+    unsolvedgrid = readrandomdata(filepath)
+    tempgrid = unsolvedgrid
+    solvedgrid = solveboard(tempgrid)
+    return unsolvedgrid, solvedgrid
+
+
+# filepath = "sudoku_boards_unsolved.txt"
+
+# unsolvedgrid = readrandomdata(filepath)
+# print("Unsolved Sudoku Grid:")
+# displaygrid(unsolvedgrid)
+
+# start_time = time.time()
+# solvedgrid = solveboard([row[:] for row in unsolvedgrid])
+# end_time = time.time()
+# elapsed_time = end_time - start_time
+
+# print("Solved Sudoku Grid:")
+# displaygrid(solvedgrid)
+# print("It took", elapsed_time, "seconds to solve this board")
